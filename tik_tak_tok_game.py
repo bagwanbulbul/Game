0bd51a86388c0f0171ceb3805a704a1558ded529
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-06-26 01:24:51 +0530

    some emplemantion in my game

--- tik_tak_tok_game.py
+++ tik_tak_tok_game.py
@@ -1,37 +1,42 @@
 print "********Tik Tok Tak Game**********"
 board = [0,1,2,
-		3,4,5,
-		6,7,8]
+ 		3,4,5,
+ 		6,7,8]
 
-def show():
+def show(board):
+	# board = [0,1,2,
+	# 3,4,5,
+	# 6,7,8]
 	print board[0],"|",board[1],"|",board[2]
 	print "----------"
 	print board[3],"|",board[4],"|",board[5]
 	print "----------"
 	print board[6],"|",board[7],"|",board[8]
-show()
+# board = [0,1,2,3,4,5,6,7,8]
+show(board)
 
-def check(char,place1,place2,place3):
+def checkline(char,place1,place2,place3):
 	if board[place1] == char and board[place2] == char and board[place3]==char:
 		return True
-def check1(char):
-	if check(char,0,1,2):
-		True
-	if check(char,1,4,7):
-		True
-	if check(char,2,5,8):
-		True
+def checkall(char):
+	if checkline(char,0,1,2):
+		return True
+	if checkline(char,1,4,7):
+		return True
+	if checkline(char,2,5,8):
+		return True
+	if checkline(char,0,3,6):
+		return True
 
-	if check(char,6,7,8):
-		True
-	if check(char,3,4,5):
-		True
-	if check(char,0,3,6):
-		True
-	if check(char,2,4,6):
-		True
-	if check(char,0,4,8):
-		True
+	if checkline(char,6,7,8):
+		return True
+	if checkline(char,3,4,5):
+		return True
+
+	if checkline(char,2,4,6):
+		return True
+	if checkline(char,0,4,8):
+		return True
 
 while True:
 	input = raw_input("select a place")
@@ -39,8 +44,7 @@ while True:
 
 	if board[input] != "x" and board[input] != "o":
 		board[input] = "x"
-
-		if check1("x") == True:
+		if checkall("x") == True:
 			print "----x is win -----"
 			break
 
@@ -50,12 +54,13 @@ while True:
 
 			if board[user_input]!="o" and board[user_input] != "x":
 				board[user_input] = "o"
+				#show()
 				break
 
-				if check1("o") == True:
+				if checkall("o") == True:
 					print "----- o is win ----"
 					break
-
 	else:
 		print "This place is already exist"
-	show()
+	show(board)
+
